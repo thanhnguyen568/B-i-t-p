a9c1237c52538fd90e8e1e1bd7ada3d6119a03c4
thanhnguyen568 <thanhnguyen230@gmail.com>
2022-08-23 22:11:28 +0700

    Add baitap file

---
+++ 11-cau-truc-lap-2/hien-thi-cac-so-nguyen-to-dau-tien.js
@@ -0,0 +1,29 @@
+function lasonguyento(n){
+    let flag = 1;
+
+    if (n <2){
+        return flag = 0;
+    }
+
+    let i = 2;
+    while(i < n){
+        if(n % i == 0) {
+            flag = 0;
+            break;
+        }
+        i++;
+    }
+
+    return flag;
+}
+
+let n = prompt(" nhap mot so n: ");
+let i = 0, check = 0, result = "";
+while (i < n){
+    check = lasonguyento(i);
+    if( check == 1 ){
+        result += i + " ";
+    }
+    ++i;
+}
+console.log(result);
