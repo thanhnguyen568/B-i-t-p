82927a3a1db1aa43127a3d16888e97e4165dd947
thanhnguyen568 <thanhnguyen230@gmail.com>
2022-08-24 19:33:28 +0700

    Add baitap file

--- 11-cau-truc-lap-2/luyen-tap-vong-lap-1.js
+++ 11-cau-truc-lap-2/luyen-tap-vong-lap-1.js
@@ -1,18 +1,21 @@
-//--BÀI 1--
+//--BÀI 1--//
 function bai_1() {
+    let result
 
     for (i = 1; i <= 100; i++){
-        document.write(i + ", ");
+        result = result +"," +i
         if (i == 99){
-        alert("Bài 1 hoàn thành!");
-        break;
+            alert(result);
+            alert("Bài 1 hoàn thành!");
+            break;
         }
     }
 }
 
-//--BÀI 2--
+//--BÀI 2--//
 function bai_2() {
     let temperature = parseInt(prompt("Nhập nhiệt độ vào: "));
+
     while (temperature < 100 || temperature > 20){
         if (temperature > 100){
             alert("Giảm nhiệt độ xuống dưới 100!");
@@ -22,22 +25,69 @@ function bai_2() {
             alert("Tăng nhiệt độ lên trên 20!");
             temperature = prompt("Nhập nhiệt độ vào: ");
         }
-        alert("Nhiệt độ phù hợp!");
-        document.write("20 < " + temperature + " < 100");
+        alert("Nhiệt độ nhập vào phù hợp điều kiện ! 20 < " + temperature + " < 100");
         break;
     }
 }
-//--BÀI 3--
+
+//--BÀI 3--//
 function bai_3() {
-    let a=1,b=1,c, i=2;
-    let number=prompt('So luong so fibonacci muon nhan la');
-    let fibonacci='1-1';
-    while (i<number) {
-        c=a+b;
-        fibonacci=fibonacci+'-'+c;
-        a=b;
-        b=c;
+    let F1 = 1, F2 = 1, Fi, i=2;
+    let number = prompt('Số lượng trong dãy fibonaci cần in ra: ');
+    let result_fibonaci = "1; 1";
+
+    while (i < number) {
+        Fi = F1 + F2;
+        result_fibonaci = result_fibonaci +', '+Fi;
+        F1 = F2;
+        F2 = Fi;
         i++;
     }
-    document.write('Day so fibonacci nhan duoc la: <br>'+fibonacci);
+    alert("Dãy số fibonaci: " +result_fibonaci);
+}
+
+//--BÀI 4--//
+function bai_4() {
+    //so dau tien
+    let F1 = 1;
+    //so thu 2
+    let F2 = 1;
+    //tong 2 so lien tiep cuoi cung
+    let F
+    //bien tang
+    let i = 2;
+
+    while (i > 0){
+        F = F1 +F2;
+        F1 = F2;
+        F2 = F;
+        i++;
+        if (F % 5 == 0){
+            alert("Số chia hết cho 5 đầu tiên trong dãy Fibonaci là: " +F);
+            break;
+        }
+    }
+}
+
+//--BÀI 5--//
+function bai_5() {
+    //so dau tien
+    let F1 = 1;
+    //so thu 2
+    let F2 = 1;
+    //tong 2 so lien tiep cuoi cung
+    let F
+    //bien tang
+    let i = 2;
+    //bien tinh tong
+    let sum_fibonaci =0;
+
+    while (i <= 20){
+        F = F1 + F2;
+        sum_fibonaci = F1 + F2 + F;
+        F1 = F2;
+        F2 = F;
+        i++;
+    }
+    alert(sum_fibonaci);
 }
