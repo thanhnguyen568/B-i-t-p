eae8816755d3b5bedadbd9f0a01f2520ed46ec9a
thanhnguyen568 <thanhnguyen230@gmail.com>
2022-08-24 17:46:57 +0700

    Add baitap file

--- 11-cau-truc-lap-2/hien-thi-cac-so-nguyen-to-dau-tien.js
+++ 11-cau-truc-lap-2/hien-thi-cac-so-nguyen-to-dau-tien.js
@@ -4,20 +4,20 @@ let numbers = prompt("Nhap so luong so nguyen to can in ra: ");
 //biến đếm số nguyên tố
 let count = 0;
 
-//biến tăng nhận giá trị sô nguyên tố
+//biến nhận giá trị sô nguyên tố tăng
 let n = 2;
 
 let prime = "";
 
 while (count < numbers){
-    let flag = true;
+    let check = true;
 
         for (let i = 2; i <= Math.sqrt(n); i++){
             if(n % i === 0) {
-                flag = false;
+                check = false;
             }
         }
-        if(flag == true){
+        if(check == true){
             count++;
             prime = prime + n + " ";
         }
