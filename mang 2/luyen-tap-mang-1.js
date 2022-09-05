99eef7a2adc424e3c6453e48f3fad21458efc4a5
thanhnguyen568 <thanhnguyen230@gmail.com>
2022-09-05 19:19:23 +0700

    Add baitap file

--- mang 2/luyen-tap-mang-1.js
+++ mang 2/luyen-tap-mang-1.js
@@ -1,12 +1,12 @@
 // Bai 1 tinh va hien thi so nguyen lon hon 10
 function array1() {
-    let arrb1 = [12, 13, 1, 67, -123, 76, 24, 56, 2, 5];
-    console.log(arrb1);
+    let arr_b1 = [12, 13, 1, 67, -123, 76, 24, 56, 2, 5];
+    console.log(arr_b1);
     let result = "";
 
-    for (let i = 0; i < arrb1.length; i++) {
-        if (arrb1[i] >= 10) {
-            result = arrb1[i];
+    for (let i = 0; i < arr_b1.length; i++) {
+        if (arr_b1[i] >= 10) {
+            result = arr_b1[i];
         } else {
             result = "";
         }
@@ -17,19 +17,50 @@ function array1() {
 
 // Bai 2 hien thi phan tu lon nhat trong mang
 function array2() {
-    let arrb2 = [23, 1, 54, 78, 15, -65, 32, 21, 85, 30];
+    let arr_b2 = [23, 1, 54, 78, 15, -65, 32, 21, 85, 30];
     let max = "";
     let index = "";
 
-    for (let i = 0; i < arrb2.length; i++) {
-        if (arrb2[i] > arrb2[i - 1]) {
-            max = arrb2[i];
+    for (let i = 0; i < arr_b2.length; i++) {
+        if (arr_b2[i] > arr_b2[i - 1]) {
+            max = arr_b2[i];
             index = i;
         } else {
-            max = arrb2[i - 1];
+            max = arr_b2[i - 1];
             index = (i - 1);
         }
     }
     alert("Phan tu lon nhat la: " + max);
-    alert("Vi tri cua phan tuy la: " + index);
+    alert("Vi tri cua phan tu la: " + index);
+}
+
+// Bai 5 diem so nguyen am trong 1 chuoi
+function array5() {
+    let array_b5 = [23, 1, 54, -78, 15, -65, 32, 21, 85, -30];
+    console.log(array_b5);
+    let count = 0;
+
+    for (let i = 0; i < array_b5.length; i++) {
+        if (array_b5[i] < 0) {
+            count++;
+        }
+    }
+    alert("Co " + count + " so nguyen am trong mang");
+}
+
+// Bai 6
+function array6() {
+    let arr_b6 = [23, 65, 45, 87, 26, 25, 78, 96, 5, 47];
+    let num_v = parseInt(prompt("Nhap vao so V bat ky: "));
+    let check = false
+
+    for (let i = 0; i < arr_b6.length; i++) {
+        if (num_v === arr_b6[i]) {
+            check = true;
+            document.writeln("V is in the array");
+        }
+    }
+    if (check === false) {
+        document.writeln("V is not in the array")
+    }
 }
