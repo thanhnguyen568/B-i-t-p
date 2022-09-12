ef8c19c0c8672da077c83a6c7d213bfa19351343
thanhnguyen568 <thanhnguyen230@gmail.com>
2022-09-12 19:05:27 +0700

    Add baitap file

--- 9-cau-truc-dieu-kien-2/bai-tap-may-tinh-don-gian.js
+++ 9-cau-truc-dieu-kien-2/bai-tap-may-tinh-don-gian.js
@@ -1,11 +1,13 @@
-function cleanscreen() {
+function cleanScreen() {
     document.getElementById("result").value = "";
 }
-function display() {
-    document.getElementById("result").value = +value;
+
+function display(value) {
+    document.getElementById("result").value += value;
 }
+
 function calculate() {
     let a = document.getElementById("result").value;
-    b = eval(a);
-    document.getElementById("result").value = a
+    let b = eval(a);
+    document.getElementById("result").value = b;
 }
