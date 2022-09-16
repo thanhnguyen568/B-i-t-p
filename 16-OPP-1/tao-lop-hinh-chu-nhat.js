9a010bccb48533dd772c8d00b39a82507c83a112
thanhnguyen568 <thanhnguyen230@gmail.com>
2022-09-16 18:45:02 +0700

    Add baitap file

---
+++ 16-OPP-1/tao-lop-hinh-chu-nhat.js
@@ -0,0 +1,32 @@
+class rectangle {
+    constructor(a, b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    area() {
+        let s = this.a * this.b;
+        document.writeln("Dien tich hinh chu nhat la: " + s);
+        document.writeln("<br>");
+    }
+
+    perimeter() {
+        let c = (this.a + this.b) * 2;
+        document.writeln("Chu vi hinh chu nhat la: " + c);
+        document.writeln("<br>");
+    }
+
+    canvas() {
+        let canvas = document.getElementById("myCanvas");
+        let ctx = canvas.getContext("2d");
+        ctx.fillStyle = "#00FFFF";
+        ctx.fillRect(25,25,this.a,this.b);
+    }
+}
+
+let recA = new rectangle(parseInt(prompt("Nhap do dai canh a < 400")),parseInt(prompt("Nhap do dai canh b < 400")));
+console.log(recA);
+
+recA.area();
+recA.perimeter();
+recA.canvas()
